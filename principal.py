75a2621c3b9e3834a17cf623c0941190089b35e2
LuizCamargos <46843342+LuizCamargos@users.noreply.github.com>
2019-03-28 08:30:26 -0300

    Comentarios iniciais

---
+++ principal.py
@@ -0,0 +1,6 @@
+/////////////////////////////Regras/////////////////////////////////////
+// nao utilizar caracteres especiais, somente os que existem no ingles//
+// sempre comentar os codigos para facilitar o entendimento           //
+// seguir o guia de estilo PEP8 (padronizacao do codigo)              //
+// varaveis, funcoes, arquivos... escritos em portugues               //
+////////////////////////////////////////////////////////////////////////
